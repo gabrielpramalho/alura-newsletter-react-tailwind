6ff5ed73a3246bbc0a8f958e816aaec102917c21
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-10-23 17:57:28 -0300

    add component Article

--- src/components/ArticleList/ArticleList.jsx
+++ src/components/ArticleList/ArticleList.jsx
@@ -1,10 +1,11 @@
 import data from '../../../articles.json'
+import Article from '../Article/Article'
 
 const ArticleList = () =>{
     return(
-        <div className='mt-5 grid sm:mt-0 m-auto max-w-2xl gap-5 sm:grid-cols-2 lg: grid-cols-3'>
+        <div className='mt-5 grid sm:mt-0 m-auto max-w-2xl gap-5 sm:grid-cols-2 lg: grid-cols-3 lg:max-w-[1000px]'>
         {
-            data.map( props => <p>{props.title}</p>)
+            data.map( (props, index) => <Article key={index} {...props}  /> )
         }
         </div>
     )
