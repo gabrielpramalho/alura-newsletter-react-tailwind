47862ec49953d5c9a1ec2d6da81dc8e2b601cd3e
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-10-24 09:59:11 -0300

    add Form

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,24 @@
+import Input from "../Input/Input"
+
+const Form = ({ onSubmit }) =>{
+
+    const safeSubmit = event => {
+        event.preventDefault()
+        event.stopPropagation()
+
+        const name = event.target[0].value
+        const email = event.target[1].value
+
+        onSubmit({ name, email })
+    }
+
+    return(
+        <form onSubmit={safeSubmit} className="flex flex-col items-center justify-center h-full gap-10 -mt-20 mx-5" >
+            <Input type="text" required placeholder="Insira seu nome" />
+            <Input type="email" required placeholder="Insira seu e-mail" />
+            <button type="submit" className="py-1 px-5 w-full max-w-xs bg-alura-100 dark:bg-dark-200 rounded-full text-gray-200 outline-none" >Seguir</button>
+        </form>
+    )
+}
+
+export default Form
