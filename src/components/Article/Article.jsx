bf6145701240d8aab1b3a3eea3ef6719136e0f28
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-10-24 09:31:24 -0300

    add tags

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,7 +1,20 @@
 const Article = ({ title, text, tags, image, alt }) =>{
     return(
-        <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center">
+        <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center gap-2">
             <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold" >{title}</h3>
+            <div className="w-full hidden sm:flex justify-end gap-2 pr-5">
+                {
+                    tags.map( tag => 
+                        <span 
+                            key={tag} 
+                            className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs uppercase" 
+                        >
+                                {tag}
+                        
+                        </span>
+                    )
+                }
+            </div>
             <div className="grid gap-1">
                 {
                     text.map((content, index) => 
