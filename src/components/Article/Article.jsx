e6bbc66a64fca347d6f3263e27dcd921bfaf2b99
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2023-10-24 10:15:05 -0300

    add pseudo classes

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,13 +1,13 @@
 const Article = ({ title, text, tags, image, alt }) =>{
     return(
-        <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center gap-2">
+        <div className="p-5 bg-gray-200 dark:bg-dark-200 sm:rounded-xl sm:shadow-lg flex flex-col items-center gap-2 sm:hover:shadow-lg sm:hover:shadow-gray-600 sm:hover:dark:shadow-black ">
             <h3 className="text-xl text-alura-200 dark:text-gray-200 font-bold" >{title}</h3>
             <div className="w-full hidden sm:flex justify-end gap-2 pr-5">
                 {
                     tags.map( tag => 
                         <span 
                             key={tag} 
-                            className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs uppercase" 
+                            className="bg-alura-100 dark:bg-dark-100 px-4 py-1 rounded-full text-gray-200 font-bold text-xs uppercase hover:bg-alura-200 hover:scale-110" 
                         >
                                 {tag}
                         
--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,7 +1,7 @@
 const Input = props => {
     return(
         <input
-            className=" py-1 pl-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none" 
+            className=" py-1 pl-5 w-full max-w-lg dark:bg-dark-200 text-dark-200 dark:text-gray-200 rounded-full outline-none placeholder:text-gray-400 placeholder:dark:text-gray-600 placeholder:text-xs placeholder:font-bold focus:border-2 focus:border-alura-100 focus:invalid:border-2 focus:invalid:border-red-600 focus:invalid:dark:border-2 focus:invalid:dark:border-red-600" 
             {...props}    
         />
     )
